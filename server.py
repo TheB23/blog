0827741c178446ddf0e7711810705c7c878f410a
TheB23 <m.aladdin23x@gmail.com>
2025-01-21 11:28:10 +0000

    Update server and front-end, added index

--- server.py
+++ server.py
@@ -2,16 +2,21 @@ from flask import Flask , render_template
 import requests 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-	return f"Yup working!!!"
-
-@app.route("/blog")
-def blog():
+def blog_api():	
 	base_url = "https://api.npoint.io/c790b4d5cab58020d391"
 	response = requests.get(base_url)
 	data = response.json()
-	return render_template("blog.html", posts = data, num = 10)
+	return data
+
+@app.route("/")
+def home():
+	blog_data = blog_api()
+	return render_template("index.html", posts = blog_data)
+@app.route("/blog/<title>")
+def blog(title):
+	print(title)
+	blog_data = blog_api()
+	return render_template("blog.html", posts = blog_data)
 
 if __name__ == "__main__":
 	app.run(debug=True)
